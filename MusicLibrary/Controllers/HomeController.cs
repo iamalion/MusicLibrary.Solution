82674380a76c77f8ca1ae260e8c1d6a21a3d4fac
Lindsay Warr <iamalion@gmail.com>
2023-07-20 13:36:25 -0700

    Add formand New and Create

--- MusicLibrary/Controllers/HomeController.cs
+++ MusicLibrary/Controllers/HomeController.cs
@@ -11,5 +11,18 @@ namespace MusicLibrary.Controllers
             Record newRecord = new Record("Add new record");
             return View(newRecord);
         }
+
+        [Route("/titles/new")]
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [Route("/titles")]
+        public ActionResult Create(string title)
+        {
+            Record myRecord = new Record(title);
+            return View("Index", myRecord);
+        }
     }
 }
