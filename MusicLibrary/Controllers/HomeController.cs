d163ffd0ca6444699dca88e36d67521055c4ba6a
Lindsay Warr <iamalion@gmail.com>
2023-07-20 14:49:22 -0700

    Create separate Records Controller

--- MusicLibrary/Controllers/HomeController.cs
+++ MusicLibrary/Controllers/HomeController.cs
@@ -11,19 +11,6 @@ namespace MusicLibrary.Controllers
             return View(Record.instances);
         }
 
-        [Route("/records/new")]
-        public ActionResult New()
-        {
-            return View();
-        }
-
-
-        //view should have same type (list)
-        [Route("/records")]
-        public ActionResult Create(string title, string artist, int year)
-        {
-            Record myRecord = new Record(title, artist, year);
-            return View("Index", myRecord);
-        }
+        
     }
 }
